48e647a801ec61f2b90d128bbca7406b45166b65
Kiera <11018737@uvu.edu>
2023-09-06 10:43:11 -0600

    2 Ex. of Challenge 3 and corrected variables in 2

--- Cs-Unit-1a-Challenge.cs
+++ Cs-Unit-1a-Challenge.cs
@@ -7,10 +7,14 @@ Console.WriteLine(myNum);
 string name = "Anime";
 Console.WriteLine(name);
 
-int x = 30;
-int y = 6;
-Console.WriteLine(x + y);
+int z = 30;
+int q = 6;
+Console.WriteLine(z + q);
 
 int x = 10;
 int y = 6;
 Console.WriteLine(x * y);
+
+Console.WriteLine("Am I doing this right?");
+
+Console.WriteLine("Did it work?");
